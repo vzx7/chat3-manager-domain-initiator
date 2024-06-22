d27e586c0b98159c0664dc5cd3111bc356870242
vzx7 <coder.vz@gmail.com>
2024-06-22 18:05:18 +0300

    добавлены переменные ENV

--- initiator.sh
+++ initiator.sh
@@ -10,21 +10,12 @@ if [ $# -ne 5 ]
 fi
 
 PSQL=/usr/bin/psql
-
-DB_USER=root
 DB_HOST=localhost
-DB_NAME=dev
-
-DOMAIN_USER=$1
-DOMAIN_IP=$2
-DOMAIN_BASE=$3
-DOMAIN_NS_SERVER_1=$4
-DOMAIN_NS_SERVER_2=$5
 
 $PSQL \
     -X \
     -h $DB_HOST \
-    -U $DB_USER \
+    -U $PG_USER_CHAT3_MANAGER \
     -w \
     -c "select \"id\", \"domain\" from services where \"isInitialization\" = false" \
     --single-transaction \
@@ -34,28 +25,28 @@ $PSQL \
     -t \
     --field-separator ' ' \
     --quiet \
-    -d $DB_NAME \
+    -d $PG_BD_CHAT3_MANAGER \
 | while read id domain; do
     # Add a domain using the hestia CLI API
-    v-add-web-domain $DOMAIN_USER "$domain.$DOMAIN_BASE" $DOMAIN_IP yes "www.$domain.$DOMAIN_BASE";
+    v-add-web-domain $CHAT3_DOMAIN_USER "$domain.$CHAT3_DOMAIN_BASE" $CHAT3_DOMAIN_IP yes "www.$domain.$CHAT3_DOMAIN_BASE";
     if [ $? -ne 0 ]
         then
         echo "Could not create domain: $domain">>error.log
         exit 1
     fi
-    v-add-dns-domain $DOMAIN_USER "$domain.$DOMAIN_BASE" $DOMAIN_IP $DOMAIN_NS_SERVER_1 $DOMAIN_NS_SERVER_2;
+    v-add-dns-domain $CHAT3_DOMAIN_USER "$domain.$CHAT3_DOMAIN_BASE" $CHAT3_DOMAIN_IP $CHAT3_DOMAIN_NS_SERVER_1 $CHAT3_DOMAIN_NS_SERVER_2;
     if [ $? -ne 0 ]
         then
         echo "Could not create DNS for domain: $domain">>error.log
         exit 1
     fi
-    v-add-letsencrypt-domain $DOMAIN_USER "$domain.$DOMAIN_BASE" "www.$domain.$DOMAIN_BASE"
+    v-add-letsencrypt-domain $CHAT3_DOMAIN_USER "$domain.$CHAT3_DOMAIN_BASE" "www.$domain.$CHAT3_DOMAIN_BASE"
     if [ $? -ne 0 ]
         then
         echo "Could not create ssl cert for domain:$domain">>error.log
         exit 1
     fi
-    v-add-web-domain-ssl-force $DOMAIN_USER "$domain.$DOMAIN_BASE"
+    v-add-web-domain-ssl-force $CHAT3_DOMAIN_USER "$domain.$CHAT3_DOMAIN_BASE"
         if [ $? -ne 0 ]
         then
         echo "Could not create ssl forse for domain:$domain">>error.log
@@ -64,11 +55,11 @@ $PSQL \
     $PSQL \
         -X \
         -h $DB_HOST \
-        -U $DB_USER \
+        -U $PG_USER_CHAT3_MANAGER \
         -w \
         -c "update services set \"isInitialization\" = true where \"id\" = $id" \
         --quiet \
-        -d $DB_NAME
+        -d $PG_BD_CHAT3_MANAGER
 done
 
 exit;
