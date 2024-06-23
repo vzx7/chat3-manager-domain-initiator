34b32265211e6c2f6af0245a29740ee1f0c991b5
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-06-23 16:16:30 +0300

    Исправлены условия добавления домена

--- initiator.sh
+++ initiator.sh
@@ -22,30 +22,34 @@ $PSQL \
     -d $PG_BD \
 | while read id domain; do
     # Add a domain using the hestia CLI API
-    v-add-web-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" $HESTIA_DOMAIN_IP yes "www.$domain.$HESTIA_DOMAIN_BASE";
-    if [ $? -ne 0 ]
-        then
-        echo "Could not create domain: $domain">>error.log
-        exit 1
+    if v-add-web-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" $HESTIA_DOMAIN_IP yes "www.$domain.$HESTIA_DOMAIN_BASE"; then
+       echo "Domain $domain created!">>success.log
+       else 
+       echo "Could not create domain: $domain">>error.log
+       exit 1;
     fi
-    v-add-dns-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" $HESTIA_DOMAIN_IP $HESTIA_DOMAIN_NS_SERVER_1 $HESTIA_DOMAIN_NS_SERVER_2;
-    if [ $? -ne 0 ]
-        then
-        echo "Could not create DNS for domain: $domain">>error.log
-        exit 1
+    # Add DNS note
+    if v-add-dns-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" $HESTIA_DOMAIN_IP $HESTIA_DOMAIN_NS_SERVER_1 $HESTIA_DOMAIN_NS_SERVER_2; then
+       echo "Domain $domain created!">>success.log
+       else 
+       echo "Could not create DNS for domain: $domain">>error.log
+       exit 1;
     fi
-    v-add-letsencrypt-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" "www.$domain.$HESTIA_DOMAIN_BASE"
-    if [ $? -ne 0 ]
-        then
-        echo "Could not create ssl cert for domain:$domain">>error.log
-        exit 1
+    # Add ssl for domain
+    if v-add-letsencrypt-domain $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" "www.$domain.$HESTIA_DOMAIN_BASE"; then
+       echo "Domain $domain created!">>success.log
+       else 
+       echo "Could not create ssl cert for domain:$domain">>error.log
+       exit 1;
     fi
-    v-add-web-domain-ssl-force $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE"
-        if [ $? -ne 0 ]
-        then
-        echo "Could not create ssl forse for domain:$domain">>error.log
-        exit 1
+    # Add forse ssl
+    if v-add-web-domain-ssl-force $HESTIA_DOMAIN_USER "$domain.$HESTIA_DOMAIN_BASE" then
+       echo "Domain $domain created!">>success.log
+       else 
+       echo "Could not create ssl forse for domain:$domain">>error.log
+       exit 1;
     fi
+    # update isInitialization for domain
     $PSQL \
         -X \
         -h $DB_HOST \
